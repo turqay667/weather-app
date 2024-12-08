3cca10139ceeae916c166a7d392519c3c7c7fdf1
turgaymammadov <memmedovturqay871@gmail.com>
2024-12-08 12:35:53 +0400

    dd

--- src/Hourly.js
+++ src/Hourly.js
@@ -5,24 +5,30 @@ import  {Swiper,SwiperSlide}  from "swiper/react";
 const Hourly=({hourly})=>{
  
     return (
+
   <div>
         { hourly && hourly.length>0 ? (
+             
             
             <Swiper slidesPerView={4}
             spaceBetween={10}
            pagination={true}
             >
              {hourly.map((data, index)=>{
+              const times=new Date().getHours()
+            const  time=parseInt(data.time.replace(/[1-9][0-9][0-9]{2}-([0][1-9]|[1][0-2])-([1-2][0-9]|[0][1-9]|[3][0-1])/gm, ''))
+             if(time>=times){
+    
                   return (
                     <>
                       <SwiperSlide  className="hours card_box" key={index}>
-        <div className="day">{data.time.replace(/[1-9][0-9][0-9]{2}-([0][1-9]|[1][0-2])-([1-2][0-9]|[0][1-9]|[3][0-1])/gm, '')}</div>
-   
+        <div className="day">{time}:00</div>   
         <a className="icon">{data.condition.text.includes('rain') ? <FaCloudRain/> : data.condition.text.includes('cloudy')  ? ( <FaCloud/> ) : <IoIosSunny/>}</a>
 <div className="temperature">{(data.temp_c).toFixed(0)}°C</div>
                       </SwiperSlide>      
               </>
                    );
+                  }
                    
                })} 
                    </Swiper>
