68a76044de9b2cf86254143fd76ff8a524549f16
turgaymammadov <memmedovturqay871@gmail.com>
2025-02-08 22:40:07 +0400

    ss

--- src/Hourly.js
+++ src/Hourly.js
@@ -14,6 +14,8 @@ const Hourly=({hourly, degree, temp})=>{
       dots: false,
       slidesToShow: 6,
       slidesToScroll: 1,
+      autoplay:true,
+      autoplaySpeed:1000,
       responsive:[
         {
           breakpoint:1024,
